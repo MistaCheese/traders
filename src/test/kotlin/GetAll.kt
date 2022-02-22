e8008f615e89e9304d95a57f9c53835c0b790c92
MistaCheese <tylimovalex@gmail.com>
2022-02-22 12:24:34 +0300

    перенос теста на by bit

--- src/test/kotlin/GetAll.kt
+++ src/test/kotlin/GetAll.kt
@@ -2,51 +2,62 @@ package com.main
 
 import com.mashape.unirest.http.HttpResponse
 import com.mashape.unirest.http.Unirest
-import okhttp3.*
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import org.json.JSONArray
+import org.json.JSONObject
 import java.util.concurrent.TimeUnit
 
 
 class getAll {
     private val client: OkHttpClient = OkHttpClient.Builder()
-        .connectTimeout(10, TimeUnit.SECONDS)
-        .writeTimeout(10, TimeUnit.SECONDS)
-        .readTimeout(10, TimeUnit.SECONDS)
+        .connectTimeout(20, TimeUnit.SECONDS)
+        .writeTimeout(20, TimeUnit.SECONDS)
+        .readTimeout(20, TimeUnit.SECONDS)
         .build()
 
     fun get(): Double {
-        val arrayList: ArrayList<String>
-        val arrayList1: ArrayList<String> = ArrayList()
+        val currentTimestamp = System.currentTimeMillis() / 1000 // Время на by bit в секундах, млс не подходят
 
         val request = Request.Builder()
-            .url("https://www.binance.com/api/v3/klines?limit=35&symbol=BTCUSDT&interval=15m")
+            .url("https://api2.bybit.com/public/linear/market/kline?symbol=BTCUSDT&resolution=15&from=" + (currentTimestamp - 31500) + "&to=$currentTimestamp")
             .build()
+        println(currentTimestamp)
         client.newCall(request).execute().use { response ->
+            val json = JSONObject(response.body()?.string()).get("result") as JSONObject
+            val list = JSONArray(json.get("list").toString())
+            println(list)
 
-            arrayList = response.body()?.string()?.split("],[") as ArrayList<String>
-        }
-        for (i in arrayList) {
-            arrayList1.add(i.split(",")[2].replace("\"", "") + "," + i.split(",")[3].replace("\"", ""))
-        }
-        var sum5: Double = 0.0
-        var sum34: Double = 0.0
-        arrayList1.removeLast()
+            var sum5 = 0.0
+            var sum34 = 0.0
 
-        for ((i, j) in arrayList1.reversed().withIndex()) {
-
-            if (i <= 4) {
-
-                sum5 += (j.split(",")[0].toDouble() + j.split(",")[1].toDouble()) / 2
+            for ((i, j) in list.reversed().withIndex()) { // Перебор массива с конца
+                println(j)
+                if (i != 0) {
+                    if (i <= 5) { // среднее арифметическое за 5 свечей
+                        sum5 += (JSONObject(j.toString()).get("high").toString()
+                            .toDouble() + JSONObject(j.toString()).get("low").toString()
+                            .toDouble()) / 2
+                    }
+                    // среднее арифметическое за 35 свечей
+                    sum34 += (JSONObject(j.toString()).get("high").toString().toDouble() + JSONObject(j.toString()).get(
+                        "low"
+                    ).toString()
+                        .toDouble()) / 2
+                    if (i == 35) { // Если свейчей больше чем 35
+                        break
+                    }
+                }
             }
-            sum34 += (j.split(",")[0].toDouble() + j.split(",")[1].toDouble()) / 2
 
+            sum5 /= 5
+            sum34 /= 34
+            println(sum5)
+            println(sum34)
+            println(sum5 - sum34)
+            return sum5 - sum34
 
         }
-        sum5 /= 5
-        sum34 /= 34
-//        println(sum5)
-//        println(sum34)
-//        println(sum5 - sum34)
-        return sum5 - sum34
     }
 
     fun sendMessage(message: String, userID: String) {
